a16db32251f5375b712a060f6b0abe4877d7d59f
Kenny Meyer <kenny@kennymeyer.net>
2020-11-14 20:18:26 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,153 @@
-import logo from './logo.svg';
-import './App.css';
+import kodi from "kodi-websocket";
+import { useCallback, useEffect, useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+
+import "./App.scss";
+import LogoCDINPY from "./assets/logo-cdinpy.png";
+import LogoJica from "./assets/logo-jica.png";
+import LogoAsociacion from "./assets/logo asociación.png";
+
+function sleep(ms) {
+  return new Promise((res) => {
+    setTimeout(function () {
+      res();
+    }, ms);
+  });
+}
+
+/* Utility function to stop all active players of a kodi instance */
+async function stopAllActivePlayers(con) {
+  const players = await con.Player.GetActivePlayers();
+
+  await Promise.all(
+    players.map((player) => {
+      con.Player.Stop(player.playerid);
+    })
+  );
+}
 
 function App() {
+  const [currentMenu, setCurrentMenu] = useState(null);
+  const [connection, setConnection] = useState(null);
+
+  const initializeConnection = useCallback(async () => {
+    const con = await kodi("192.168.1.109", 9090);
+    setConnection(con);
+  }, []);
+
+  const playSlideshow = async (folderName) => {
+    await stopAllActivePlayers(connection);
+
+    await connection.Player.Open({
+      item: { directory: folderName },
+    });
+  };
+
+  const playDemo = useCallback(async () => {
+    /* Stop all players, then start the video */
+    await stopAllActivePlayers(connection);
+
+    const movies = await connection.Files.GetDirectory({
+      directory: "/storage/videos/",
+      media: "video",
+    });
+
+    // console.log("movies", movies);
+    // console.log("file", movies?.files?.[0]?.file);
+
+    await connection.Player.Open({
+      item: { file: movies?.files?.[0]?.file },
+    });
+
+    /* Stop the video after 20 seconds */
+    // await sleep(20000);
+    // await stopAllActivePlayers(connection);
+  }, []);
+
+  const MENU_INMIGRACION = [
+    {
+      name: "Documental",
+      action: () => playDemo(),
+    },
+    {
+      name: "Galería de Fotos",
+      action: () => playSlideshow("/storage/images/inmigración"),
+    },
+  ];
+
+  const MAIN_MENU = [
+    {
+      name: "Inmigración a Paraguay",
+      action: () => setCurrentMenu(MENU_INMIGRACION),
+    },
+    {
+      name: "Kouresha Shakai",
+      action: () => playDemo(),
+    },
+    {
+      name: "Lenguaje Nikkei",
+      action: () => playDemo(),
+    },
+    {
+      name: "Nihongogakko",
+      action: () => playDemo(),
+    },
+    {
+      name: "Música Nikkei",
+      action: () => playDemo(),
+    },
+    {
+      name: "Taiko - Yosakoi",
+      action: () => playDemo(),
+    },
+  ];
+
+  useEffect(() => {
+    console.log("connection changed", connection);
+  }, [connection]);
+
+  useEffect(() => {
+    setCurrentMenu(MAIN_MENU);
+    initializeConnection();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="backgroundArt" />
+
+      <img
+        src={LogoCDINPY}
+        style={{ width: 600, marginBottom: 40 }}
+        alt="logo"
+      />
+
+      <Container fluid>
+        <Row>
+          <Col>
+            <p>
+              Elegir idoma: <a href="#">Español</a> / <a href="#">日本語</a>
+            </p>
+          </Col>
+        </Row>
+
+        {currentMenu?.map?.((item) => {
+          return (
+            <Row key={item.name} className="menuOption">
+              <Col>
+                <Button size="lg" onClick={item.action}>
+                  {item.name}
+                </Button>
+              </Col>
+            </Row>
+          );
+        })}
+      </Container>
+
+      <div style={{ position: "absolute", bottom: 20, right: 20 }}>
+        <img src={LogoAsociacion} style={{ height: 100 }} alt="logo" />
+        <img src={LogoJica} style={{ height: 100 }} alt="logo" />
+      </div>
+    </>
   );
 }
 
